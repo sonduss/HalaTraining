f5167545997e462e72eb729416350f981ecfe28e
HalaHajAli <halahajali2000@gmail.com>
2023-12-24 08:42:41 +0200

    edit

--- src/main/java/seleautomation/com/HalaTraining1/App.java
+++ src/main/java/seleautomation/com/HalaTraining1/App.java
@@ -4,28 +4,23 @@ package seleautomation.com.HalaTraining1;
  * Hello world!
  *
  */
-public class App 
-{
-	public static void main( String[] args )
-    {
-       // System.out.println( "Hello World!" );
+public class App {
+    public int add(int a, int b) {
+        return a + b;
     }
-	    public static int add(int a, int b) {
-	        return a + b;
-	    }
 
-	    public static int subtract(int a, int b) {
-	        return a - b;
-	    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 
-	    public static int multiply(int a, int b) {
-	        return a * b;
-	    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 
-	    public static int divide(int a, int b) {
-	        if (b == 0) {
-	            throw new IllegalArgumentException("Cannot divide by zero");
-	        }
-	        return a / b;
-	    }
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
 }
