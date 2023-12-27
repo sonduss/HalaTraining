fd6683c9a4bf9be0f654498c33e3832b22602617
HalaHajAli <halahajali2000@gmail.com>
2023-12-27 09:43:05 +0200

    edit

--- src/main/java/selenium/chrome/App.java
+++ src/main/java/selenium/chrome/App.java
@@ -1,11 +1,7 @@
 package selenium.chrome;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.Assert;
 
 public class App {
 
@@ -18,31 +14,10 @@ public class App {
             // Navigate to Google
             driver.get("https://www.google.com");
 
-            // Click inside the search box
-            WebElement searchBox = driver.findElement(By.name("q"));
-            searchBox.click();
-
-            // Type the search query
-            searchBox.sendKeys("Selenium Tutorials");
-
-            // Submit the search
-            searchBox.sendKeys(Keys.ENTER);
-
-            // Validate the title
-            String expectedTitle = "Selenium Tutorials - Google Search"; // Adjust as needed
-            String actualTitle = driver.getTitle();
-            
-            // Assert the title
-            Assert.assertEquals(expectedTitle, actualTitle);
-
-        } catch (Exception e) {
-            // Handle exceptions if needed
-            e.printStackTrace();
+            // ... (Perform search actions here)
         } finally {
             // Close the browser
-            if (driver != null) {
-                driver.quit();
-            }
+            driver.quit();
         }
     }
 }
--- src/test/java/selenium/chrome/AppTest.java
+++ src/test/java/selenium/chrome/AppTest.java
@@ -1,20 +1,38 @@
 package selenium.chrome;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.Assert;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testGoogleSearch() {
+        // Set up WebDriver (using the same setup as in the main code)
+    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\MASS\\eclipse-workspace\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            // Navigate to Google
+            driver.get("https://www.google.com");
+
+            // Perform search actions
+            WebElement searchBox = driver.findElement(By.name("q"));
+            searchBox.click();
+            searchBox.sendKeys("Selenium Tutorials");
+            searchBox.sendKeys(Keys.ENTER);
+
+            // Validate the title
+            String expectedTitle = "Selenium Tutorials - Google Search";
+            String actualTitle = driver.getTitle();
+            Assert.assertEquals(expectedTitle, actualTitle);
+        } finally {
+            // Close the browser
+            driver.quit();
+        }
     }
 }
