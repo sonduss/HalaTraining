2846114e4d9d662ee5e3e8839081ac84401314ca
HalaHajAli <halahajali2000@gmail.com>
2024-01-10 11:31:47 +0200

    task3

--- src/main/java/seleautomation/com/HalaTraining1/GitHubHomePage.java
+++ src/main/java/seleautomation/com/HalaTraining1/GitHubHomePage.java
@@ -4,10 +4,23 @@ package seleautomation.com.HalaTraining1;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class GitHubHomePage {
+
+    public static void main(String[] args) {
+    
+    }
+    private WebDriver driver;
+    private final String signInUrl = "https://github.com/login"; 
+
+    public GitHubHomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public GitHubSignInPage navigateToSignIn() {
+    	driver.get(signInUrl); 
+        return new GitHubSignInPage(driver);
     }
 }
---
+++ src/test/java/seleautomation/com/HalaTraining1/GitHubtest.java
@@ -0,0 +1,38 @@
+package seleautomation.com.HalaTraining1;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import utilities.DataReader;
+import utilities.DriverFactory;
+
+public class GitHubtest {
+
+    @DataProvider(name = "testData")
+    public Object[][] provideTestData() {
+        DataReader reader = new DataReader("\"C:\\Users\\MASS\\OneDrive\\Desktop\\QA.xlsx\""); 
+        return reader.readTestData();
+    }
+
+    @Test(dataProvider = "testData")
+    public void testSignIn(String testCaseId, String username, String password, boolean expectedResult) {
+        WebDriver driver = DriverFactory.createDriver("chrome"); 
+
+        GitHubHomePage homePage = new GitHubHomePage(driver);
+        GitHubSignInPage signInPage = homePage.navigateToSignIn();
+
+        signInPage.signIn(username, password);
+
+        // Assert sign-in results
+        if (expectedResult) {
+            Assert.assertTrue(signInPage.isSignInSuccessful(), "Sign-in failed unexpectedly.");
+        } else {
+            Assert.assertFalse(signInPage.isSignInSuccessful(), "Sign-in succeeded unexpectedly.");
+            Assert.assertEquals(signInPage.getErrorMessage(), "Incorrect username or password."); 
+        }
+
+        // Close the browser
+        driver.quit();
+    }
+}
